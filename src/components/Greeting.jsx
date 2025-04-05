d05e724ce44ff1c4581d23f60d40cc6bb00fe3f1
Jordan Higgins <jordanhiggins@usf.edu>
2025-04-05 19:38:58 -0400

    Task 1 - Created Greeting Component

---
+++ src/components/Greeting.jsx
@@ -0,0 +1,10 @@
+const Greeting = () => {
+    return (
+      <div>
+        <h2>Hello from the Greeting Component!</h2>
+        <p>This is your first custom component in React.</p>
+      </div>
+    );
+  }; // Created a greeting component that displays a message.
+
+export default Greeting;
